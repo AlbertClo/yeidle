509ffc056287fa0db9157e6ca722064a8566c1e1
Albert Clo <cloete.albert@gmail.com>
2026-06-01 05:13:17 +0200

    Make pages list the default route on app startup

--- apps/desktop/routes/web.php
+++ apps/desktop/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PageWebController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Pages/Index')->name('home');
+Route::get('/', [PageWebController::class, 'index'])->name('home');
 
 Route::get('pages', [PageWebController::class, 'index'])->name('pages.index');
 Route::get('pages/{node}', [PageWebController::class, 'show'])->name('pages.show');
