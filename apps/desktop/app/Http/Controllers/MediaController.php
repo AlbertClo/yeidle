856a49bc4d85277c6619b9cb4c03f88dc64193c6
Albert Clo <cloete.albert@gmail.com>
2026-06-01 12:14:34 +0200

    Add untested comments for macOS and Windows open commands

--- apps/desktop/app/Http/Controllers/MediaController.php
+++ apps/desktop/app/Http/Controllers/MediaController.php
@@ -143,8 +143,8 @@ class MediaController extends Controller
     {
         $arg = escapeshellarg($path);
         match (PHP_OS_FAMILY) {
-            'Darwin' => exec("open {$arg} > /dev/null 2>&1 &"),
-            'Windows' => exec("start \"\" {$arg}"),
+            'Darwin' => exec("open {$arg} > /dev/null 2>&1 &"), // untested
+            'Windows' => exec("start \"\" {$arg}"), // untested
             default => exec("xdg-open {$arg} > /dev/null 2>&1 &"),
         };
     }
